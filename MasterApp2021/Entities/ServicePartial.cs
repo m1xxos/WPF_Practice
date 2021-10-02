43150534201a6f9ba8e8bb5bbe8eec75a6a9da43
m1xxos <66390094+m1xxos@users.noreply.github.com>
2021-10-02 20:23:35 +0300

    master1

---
+++ MasterApp2021/Entities/ServicePartial.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MasterApp2021.Entities
+{
+    public partial class Service
+    {
+        public string DiscountText => (Discount == 0 || Discount == null) ? "" : $"* скидка {Discount * 100} %";
+        public string TotalCost => (Discount == 0 || Discount == null) ? $"{Cost:N2} рублей за {DurationInSeconds / 60} минут" : $"{CostWithDiscount:N2} рублей за {DurationInSeconds / 60} минут";
+
+        public double CostWithDiscount => (Discount == 0 || Discount == null) ? (double)Cost : ((double)Cost * (1.0 - Discount)).Value;
+
+        public Visibility DiscountVisibility => (Discount == 0 || Discount == null) ? Visibility.Collapsed : Visibility.Visible;
+
+        public string BackColor => (Discount == 0 || Discount == null) ? "#ffffe1" : "#d1ffd1";
+
+    }
+}
--- MasterApp2021/Pages/ServicesPage.xaml.cs
+++ MasterApp2021/Pages/ServicesPage.xaml.cs
@@ -15,14 +15,53 @@ using System.Windows.Shapes;
 
 namespace MasterApp2021.Pages
 {
-    /// <summary>
-    /// Interaction logic for ServicesPage.xaml
-    /// </summary>
-    public partial class ServicesPage : Page
-    {
-        public ServicesPage()
-        {
-            InitializeComponent();
-        }
-    }
+	/// <summary>
+	/// Interaction logic for ServicesPage.xaml
+	/// </summary>
+	public partial class ServicesPage : Page
+	{
+		public ServicesPage()
+		{
+			InitializeComponent();
+			LViewServices.ItemsSource = App.Context.Services.ToList();
+			ComboDiscount.SelectedIndex = 0;
+			ComboSortBy.SelectedIndex = 0;
+			UpdateServices();
+		}
+
+		void ComboSortBy_SelectionChanged(object sender, SelectionChangedEventArgs e) => UpdateServices();
+
+		void ComboDiscount_SelectionChanged(object sender, SelectionChangedEventArgs e) => UpdateServices();
+
+		void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e) => UpdateServices();
+
+		void UpdateServices()
+		{
+			var services = App.Context.Services.ToList();
+			int total = services.Count;
+			if (ComboSortBy.SelectedIndex == 0)
+				services = services.OrderBy(p => p.CostWithDiscount).ToList();
+			else
+				services = services.OrderByDescending(p => p.CostWithDiscount).ToList();
+			switch (ComboDiscount.SelectedIndex)
+			{
+				case 1: services = services.Where(p => p.Discount >= 0 && p.Discount < 0.05).ToList(); break;
+				case 2: services = services.Where(p => p.Discount >= 0.05 && p.Discount < 0.15).ToList(); break;
+				case 3: services = services.Where(p => p.Discount >= 0.15 && p.Discount < 0.30).ToList(); break;
+				case 4: services = services.Where(p => p.Discount >= 0.30 && p.Discount < 0.70).ToList(); break;
+				case 5: services = services.Where(p => p.Discount >= 0.70 && p.Discount <= 1).ToList(); break;
+			}
+			services = services.Where(p => p.Title.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+			LViewServices.ItemsSource = services;
+			BlockRecords.Text = $"{services.Count} из {total}";
+		}
+
+		void BtnAddService_Click(object sender, RoutedEventArgs e) {
+			NavigationService.Navigate(new AddEditServicePage());
+		}
+
+		void BtnEdit_Click(object sender, RoutedEventArgs e) { }
+
+		void BtnDelete_Click(object sender, RoutedEventArgs e) { }
+	}
 }

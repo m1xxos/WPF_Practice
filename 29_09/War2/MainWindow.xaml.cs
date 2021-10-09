526153682d425aff57c1c95e34a2fc73eb3c6e73
m1xxos <66390094+m1xxos@users.noreply.github.com>
2021-10-09 15:40:48 +0300

    4

---
+++ 29_09/War2/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace War2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        OpenFileDialog _openDialog = new OpenFileDialog();
+        Window1 wind1 = new Window1();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            _openDialog.Filter = "All Files (*.*)|*.*";
+            _openDialog.Multiselect = true;
+            if (_openDialog.ShowDialog() == true)
+            {
+                wind1.UpdateWindow(_openDialog.FileNames);
+            }
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            wind1.Show();
+        }
+    }
+}
---
+++ 29_09/War2/Window1.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace War2
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        SaveFileDialog _saveDialog = new SaveFileDialog();
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        public void UpdateWindow(string[] imagesArray)
+        {
+            foreach (string image in imagesArray)
+            {
+                listBox.Items.Add(image);
+            }
+        }
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            image.Source = new BitmapImage(new Uri(listBox.SelectedItem.ToString()));
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            _saveDialog.Filter = "Media files (*.PNG)|*.png";
+            if (_saveDialog.ShowDialog() == true)
+            {
+                Bitmap myBitmap = new Bitmap(listBox.SelectedItem.ToString());
+                myBitmap.Save(_saveDialog.FileName);
+            }
+        }
+    }
+}
